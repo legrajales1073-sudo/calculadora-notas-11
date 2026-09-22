95965d9927dfecee5b94423ff9ae32ba49508718
legrajales1073-sudo <legrajales1073@gmail.com>
2026-09-22 04:34:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function saludar() {
-    alert("Hola. Mi primera aplicación funciona.");
+function calcular() {
+
+    let nota1 = Number(document.getElementById("nota1").value);
+    let nota2 = Number(document.getElementById("nota2").value);
+    let nota3 = Number(document.getElementById("nota3").value);
+
+    let promedio = (nota1 + nota2 + nota3) / 3;
+
+    document.getElementById("resultado").innerHTML =
+        "Promedio: " + promedio.toFixed(2);
 }
